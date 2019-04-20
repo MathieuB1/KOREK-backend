d1deefcbb32cf071eceda17cb6a53898d3bda787
MathieuB1 <matb.work@gmail.com>
2019-04-20 15:53:24 +0200

    Create JWT Cookie Session for Reading Media

--- django-rest-swagger/myapp/app/settings.py
+++ django-rest-swagger/myapp/app/settings.py
@@ -241,12 +241,13 @@ REST_FRAMEWORK = {
     ),
 }
 
+
 JWT_AUTH = { 
     'JWT_AUTH_HEADER_PREFIX': 'Bearer',
     'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=4),
     'JWT_ALLOW_REFRESH': True,
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
-    #'JWT_AUTH_COOKIE': None,
+    'JWT_AUTH_COOKIE': 'jwtoken',
 }
 
 
